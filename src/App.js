c312fd9759b44d5643406e73f9c7c96adbb10ad1
raminsami <ramin.sami@ontariotechu.net>
2021-05-27 14:05:39 -0400

    app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+const api_key = "d0f5f2e135336200362af8a1a73acb17";
+const BASE_URL = "https://api.themoviedb.org/3";
+const getImage = (path) => `https://image.tmdb.org/t/p/w200/${path}`;
 
 function App() {
+  const [data, setData] = useState([]);
+  const api = axios.create({ baseURL: BASE_URL });
+  const getPopular = api.get("movie/popular", { params: { api_key } });
+  
+  useEffect(() => {
+    getPopular.then((res) => {
+      setData(res.data.results);
+    });
+  }, []);
+  
+  const clickHandle = ()=>{
+    console.log('liked');
+  }
+
+  const clickHandleDislike = ()=>{
+    console.log('disliked');
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="grid">
+          {data.map((movie) => (
+            <div className="item">
+              <img src={getImage(movie.poster_path)} />
+              <p>{movie.original_title}</p>
+              <p><button class="likebtn" onClick={clickHandle}>Like</button>
+              <button class="dislikebtn"onClick={clickHandleDislike}>Dislike</button></p>
+            
+            </div>
+          ))}
+        </div>
       </header>
     </div>
   );
